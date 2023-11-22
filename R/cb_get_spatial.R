879bd37fc96dc0600b5b151215da1d5664ea4d90
jaymwin <jmwiniarski@gmail.com>
2023-11-22 07:00:49 -0600

    added spatial layer functions

---
+++ R/cb_get_spatial.R
@@ -0,0 +1,35 @@
+
+#' Title
+#'
+#' @param layer_name
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+cb_get_spatial <- function(layer_name = NULL) {
+
+  if (layer_name == 'ca_boundary') {
+
+    layer <- cb_boundary_layers$ca_boundary
+
+  } else if (layer_name == 'epa_ecoregions') {
+
+    layer <- cb_boundary_layers$epa_ecoregions
+
+  } else if (layer_name == 'hexes') {
+
+    layer <- cb_boundary_layers$hexes
+
+  } else if (layer_name == 'nps_boundaries') {
+
+    layer <- cb_boundary_layers$nps_boundaries
+
+  } else if (layer_name == 'usfs_boundaries') {
+
+    layer <- cb_boundary_layers$usfs_boundaries
+
+  }
+
+}
---
+++ R/cb_list_spatial.R
@@ -0,0 +1,22 @@
+
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+cb_list_spatial <- function() {
+
+  tibble::tribble(
+    ~layer_name, ~description, ~type,
+    'hexes', 'hex sampling grid for Sierra Nevada region', 'shapefile',
+    'epa_ecoregions', 'EPA ecoregions (level III)', 'shapefile',
+    'usfs_boundaries', 'USFS national forest boundaries', 'shapefile',
+    'nps_boundaries', 'NPS boundaries', 'shapefile',
+    'ca_boundary', 'California border', 'shapefile'
+  ) %>%
+    dplyr::arrange(type, layer_name) %>%
+    print(n = Inf)
+
+}
