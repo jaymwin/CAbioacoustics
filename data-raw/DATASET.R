9bb26d6a91ae1a6a46f8602b3a774e654f4784e4
jaymwin <jmwiniarski@gmail.com>
2023-11-22 10:15:05 -0600

    added burn severity rasters; need to test

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -5,6 +5,7 @@ library(arcpullr)
 library(tidyverse)
 library(sf)
 library(janitor)
+library(terra)
 
 
 # california boundary -----------------------------------------------------
@@ -36,7 +37,8 @@ usfs_boundaries <-
   ) |>
   select(ADMINFORESTID:FORESTNAME) |>
   clean_names() |>
-  rename(admin_forest_id = adminforestid, forest_number = forestnumber, forest_org_code = forestorgcode, forest_name = forestname)
+  rename(admin_forest_id = adminforestid, forest_number = forestnumber, forest_org_code = forestorgcode, forest_name = forestname) |>
+  st_make_valid()
 
 
 # epa ecoregions ----------------------------------------------------------
@@ -59,6 +61,50 @@ hexes <-
   )
 
 
+# fire perimeters ---------------------------------------------------------
+
+# 2020-onward
+recent_fire_perimeters <-
+  arcpullr::get_spatial_layer(
+    url = 'https://services1.arcgis.com/jUJYIo9tSA7EHvfZ/ArcGIS/rest/services/California_Fire_Perimeters/FeatureServer/2',
+    where = "YEAR_ >= 2020",
+    out_fields = c('YEAR_', 'FIRE_NAME', 'CAUSE', 'C_METHOD')
+  ) |>
+  clean_names()
+
+
+# burn severity -----------------------------------------------------------
+
+# crop to boundary of all hexes
+hexes_dissolved <-
+  hexes |>
+  summarise()
+
+years <- 2020:2022
+
+for(i in seq_along(years)) {
+
+  # read in raster (CBI from RAVG)
+  r <- terra::rast(str_c('https://data.fs.usda.gov/geodata/rastergateway/ravg/ravg_', years[i], '_cbi4.tif'))
+  # create name to save raster object by year
+  r_name <- str_c('burn_severity_', years[i])
+
+  # crop/mask raster
+  r_mask <- terra::crop(r, hexes_dissolved |> st_transform(st_crs(r)), mask = TRUE)
+  plot(r_mask, main = paste0(years[i]))
+  # save raster object to memory
+  assign(r_name, r_mask)
+
+}
+
+# combine in a raster stack
+annual_rasters <- list(burn_severity_2020, burn_severity_2021, burn_severity_2022)
+burn_severity_rasters <- terra::rast(annual_rasters)
+
+# save raster stack
+# terra::writeRaster(burn_severity_rasters, "Data/IA_cdl_stack.tif", filetype = "GTiff", overwrite = TRUE)
+
+
 # save raw spatial data ---------------------------------------------------
 
 cb_boundary_layers <-
@@ -67,7 +113,9 @@ cb_boundary_layers <-
     epa_ecoregions = epa_ecoregions,
     usfs_boundaries = usfs_boundaries,
     nps_boundaries = nps_boundaries,
-    ca_boundary = ca_boundary
+    ca_boundary = ca_boundary,
+    recent_fire_perimeters = recent_fire_perimeters,
+    burn_severity_rasters = burn_severity_rasters
   )
 
 # this updates the /data folder
