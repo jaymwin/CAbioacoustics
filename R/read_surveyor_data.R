f2b848904110c08c299912e59d7e92403bb69b8a
jaymwin <jmwiniarski@gmail.com>
2024-01-31 10:41:49 -0600

    function to read and combine surveyor data

---
+++ R/read_surveyor_data.R
@@ -0,0 +1,27 @@
+
+#' Title
+#'
+#' @param df
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+read_surveyor_data <- function(df) {
+
+  file_type <- df |> stringr::str_extract('.csv|.xlsx')
+
+  if (file_type == ".csv") {
+
+    readr::read_csv(df) |>
+      dplyr::select(lat_WGS84, lon_WGS84, survey_night_year, survey_night_month, survey_night_day)
+
+  } else {
+
+    readxl::read_excel(df) |>
+      dplyr::select(lat_WGS84, lon_WGS84, survey_night_year, survey_night_month, survey_night_day)
+
+  }
+
+}
