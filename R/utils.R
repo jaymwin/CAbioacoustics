1de3b18351e0878ce6965d9a709debf5e99a1e9b
jaymwin <jmwiniarski@gmail.com>
2024-04-29 11:02:38 -0500

    add some util functions

---
+++ R/utils.R
@@ -0,0 +1,125 @@
+
+# function to fill in missing survey nights (i.e., nights with no effort)
+complete_survey_nights <- function(df) {
+
+  df |>
+    # complete list of dates for each cell_unit by year
+    complete(
+      group_id,
+      cell_id,
+      unit_number,
+      cell_unit,
+      study_type,
+      project_area,
+      survey_year,
+      survey_night = seq.Date(
+        as.Date(str_c(max(survey_year), start_date)),
+        as.Date(str_c(max(survey_year), end_date)),
+        by = "days"
+      ),
+      # fill in no effort
+      fill = list(
+        survey_hours = 0,
+        detection = as.numeric(NA)
+      )
+    )
+
+}
+
+
+# create a date sequence given start/end dates
+date_sequence <- function(df) {
+
+  seq.Date(as.Date(df$start_date), as.Date(df$end_date), by = 'day')
+
+}
+
+
+# create a data frame of dates based off of start/end years and dates
+create_season_dates <- function(start_year, end_year, start_date, end_date) {
+
+  df <-
+    tibble(
+      survey_year = start_year:end_year,
+      start_date = as.Date(str_c(survey_year, start_date)),
+      end_date = as.Date(str_c(survey_year, end_date))
+    )
+
+  df <-
+    df %>%
+    group_by(survey_year) %>%
+    nest() %>%
+    mutate(survey_night = map(data, date_sequence)) %>%
+    unnest(survey_night) %>%
+    ungroup() %>%
+    select(-data)
+
+}
+
+
+get_utm_crs <- function(df) {
+
+  zone <- unique(df$utm_zone)
+
+  if (zone == 10) {
+
+    crs = 26910
+
+  } else {
+
+    crs = 26911
+
+  }
+
+  return(crs)
+
+}
+
+
+hoots_same_night <- function(df, focal_date) {
+
+  n_rows <-
+    nrow(
+      df |>
+        # does it intersect ARU buffer?
+        dplyr::filter(survey_night_date == as.Date(focal_date))
+    )
+
+  if (n_rows > 0) {
+
+    hoots <- TRUE
+
+  } else {
+
+    hoots <- FALSE
+
+  }
+
+  return(hoots)
+
+}
+
+
+logit_to_probability <- function(x, reverse = FALSE) {
+
+  if (reverse == FALSE) {
+
+    # birdnet logit to 0-1 probability
+    1 / (1 + exp(-x))
+
+  } else if (reverse == TRUE) {
+
+    # 0-1 probability to birdnet logit
+    log(x /(1 - x))
+
+  }
+
+}
+
+
+st_intersects_any <- function(x, y) {
+
+  sf::st_intersects(x, y) %>%
+    purrr::map_lgl(~length(.x) > 0)
+
+}
