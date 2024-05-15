a55dd00036547eef1b055d9c2ffe34a46d58106b
jaymwin <jmwiniarski@gmail.com>
2024-05-15 12:27:17 -0500

    surveyor data documentation

--- R/cb_read_surveyor_data.R
+++ R/cb_read_surveyor_data.R
@@ -1,15 +1,21 @@
 
-#' Title
+#' Read callback survey data
 #'
-#' @param df
+#' @param path Path to surveyor data
 #'
 #' @return
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#' # path to data
+#' path <- here::here('surveyor_data_example.csv')
+#' surveyor_df <- cb_read_surveyor_data(path)
+#' }
 
-cb_read_surveyor_data <- function(df) {
+cb_read_surveyor_data <- function(path) {
 
+  # depending on how the clean data is saved, use read_csv or read_excel
   file_type <- df |> stringr::str_extract('.csv|.xlsx')
 
   if (file_type == ".csv") {
