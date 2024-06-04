d4c4a47bf0d32d26f911775b86ed0edecf01a003
jaymwin <jmwiniarski@gmail.com>
2024-06-04 07:31:09 -0500

    fixed broken segmentation function (input_flac lacking .flac at end of file name)

--- R/cb_segment_flac.R
+++ R/cb_segment_flac.R
@@ -16,7 +16,7 @@ cb_segment_flac <- function(begin_file, begin_time, end_time) {
   begin <- hms::as_hms(begin_time)
   end <- hms::as_hms(end_time)
 
-  input_flac <- stringr::str_c(here::here('raw_flacs'), begin_file, sep = '/')
+  input_flac <- stringr::str_c(here::here('raw_flacs'), '/', begin_file, '.flac')
   # name output by begin file name with begin/end times tacked on the end
   output_flac <- stringr::str_c(here::here('processed_flacs'), '/', stringr::str_remove(begin_file, '.flac'), '_', begin_time, '_', end_time, '.flac')
 
