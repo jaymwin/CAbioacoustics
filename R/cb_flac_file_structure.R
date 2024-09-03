fea0a130c0a9430030f454ead709aed2e5778de2
jaymwin <jmwiniarski@gmail.com>
2024-09-03 06:40:51 -0500

    added function to check nesting of flac files on research drive

---
+++ R/cb_flac_file_structure.R
@@ -0,0 +1,25 @@
+
+#' Check that flacs are properly nested on research drive
+#'
+#' @param network_drive
+#' @param survey_year
+#' @param regions
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+cb_flac_file_structure <- function(network_drive, survey_year, regions) {
+
+  stringr::str_c(
+    network_drive,
+    '/Acoustic_Data/ARU_Data_Raw/(ARU_Sierra_Monitoring|ARU_Sierra_Projects)/',
+    survey_year,
+    '/',
+    '(',
+    stringr::str_flatten(regions, '|'),
+    ')/G(P|R|C|M|[0-9])[0-9]{2}_V[0-9]{1}/G(P|R|C|M|[0-9])[0-9]{2}_V[0-9]{1}_C[0-9]{4}_U[0-9]{1}/G(P|R|C|M|[0-9])[0-9]{2}_V[0-9]{1}_C[0-9]{4}_U[0-9]{1}_[0-9]{8}/G(P|R|C|M|[0-9])[0-9]{2}_V[0-9]{1}_C[0-9]{4}_U[0-9]{1}_[0-9]{8}_[0-9]{6}Z.flac'
+  )
+
+}
