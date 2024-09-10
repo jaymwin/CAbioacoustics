0b414c41a2b666cc5109f510f356d7fbb298a1a5
jaymwin <jmwiniarski@gmail.com>
2024-09-10 11:13:04 -0500

    added function to set cores for running functions in parallel when using future/furrr

---
+++ R/cb_set_future_cores.R
@@ -0,0 +1,25 @@
+
+#' Set number of cores to run functions in parallel with future/furrr
+#'
+#' @param n
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+cb_set_future_cores <- function(n) {
+
+  if (missing(n)) {
+
+    message(stringr::str_glue('setting cores to {parallelly::availableCores() - 1} (available cores minus one)'))
+    future::plan(future::multisession, workers = parallelly::availableCores() - 1)
+
+  } else {
+
+    message(stringr::str_glue('setting cores to {n}'))
+    future::plan(future::multisession, workers = n)
+
+  }
+
+}
