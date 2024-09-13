efa80c98fc998ac4275003b837f63754be214140
jaymwin <jmwiniarski@gmail.com>
2024-09-13 14:29:50 -0500

    fixed typo

--- R/cb_create_selection_table.R
+++ R/cb_create_selection_table.R
@@ -16,7 +16,7 @@ cb_create_selection_table <- function(df, date_time) {
     Selection = df$selection,
     View = 'Spectrogram 1',
     Channel = 1,
-    `Begin File` = str_c(df$json, '.flac'),
+    `Begin File` = stringr::str_c(df$json, '.flac'),
     `Begin Time (s)` = df$relative_time,
     `End Time (s)` = df$relative_time + 3,
     `Low Freq (Hz)` = 150,
