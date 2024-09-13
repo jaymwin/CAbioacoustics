0bbf933c70ae44fdfabd4a4a1da3727c9f804ab0
jaymwin <jmwiniarski@gmail.com>
2024-09-13 14:26:10 -0500

    function to create Raven selection tables from BirdNET outputs

---
+++ R/cb_create_selection_table.R
@@ -0,0 +1,41 @@
+
+#' Title
+#'
+#' @param df
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+cb_create_selection_table <- function(df, date_time) {
+
+  flac_name <- unique(df$json)
+
+  tibble::tibble(
+    Selection = df$selection,
+    View = 'Spectrogram 1',
+    Channel = 1,
+    `Begin File` = str_c(df$json, '.flac'),
+    `Begin Time (s)` = df$relative_time,
+    `End Time (s)` = df$relative_time + 3,
+    `Low Freq (Hz)` = 150,
+    `High Freq (Hz)` = 12000,
+    `Common Name` = df$common_name,
+    Score = df$score,
+    Detector = 'BirdNET',
+    Species = df$species,
+    `Call Type` = df$call,
+    Sex = NA,
+    Keep = 'Y',
+    Overwrite = 'no'
+  ) |>
+    write.table(
+      file = stringr::str_glue(here::here('code_outputs/csow_bdow_selection_tables_{date_time}/{flac_name}.BIRDNET')),
+      sep = "\t",
+      row.names = FALSE,
+      col.names = TRUE,
+      quote = FALSE
+    )
+
+}
