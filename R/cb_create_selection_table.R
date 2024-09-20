2e23ff94ffa0e8c2618c6620e179d8ba68c07ec6
jaymwin <jmwiniarski@gmail.com>
2024-09-20 10:18:29 -0500

    more changes to selection table function

--- R/cb_create_selection_table.R
+++ R/cb_create_selection_table.R
@@ -12,6 +12,8 @@ cb_create_selection_table <- function(df, date_time, species_group) {
 
   flac_name <- unique(df$json)
 
+  if (species_group == 'csow_bdow') {
+
   tibble::tibble(
     Selection = df$selection,
     View = 'Spectrogram 1',
@@ -24,18 +26,47 @@ cb_create_selection_table <- function(df, date_time, species_group) {
     `Common Name` = df$common_name,
     Score = df$score,
     Detector = 'BirdNET',
-    Species = df$species,
+    Species = as.character(NA),
     `Call Type` = df$call_type,
-    Sex = NA,
+    Sex = as.character(NA),
     Keep = 'Y',
     Overwrite = 'no'
   ) |>
     write.table(
-      file = stringr::str_glue(here::here('code_outputs/{species_group}_selection_tables_{date_time}/{flac_name}.BirdNET')),
+      file = stringr::str_glue(here::here('code_outputs/csow_bdow_selection_tables_{date_time}/{flac_name}.BirdNET')),
       sep = "\t",
       row.names = FALSE,
       col.names = TRUE,
       quote = FALSE
     )
+
+  }
+
+  if (species_group == 'forest_owl') {
+
+    tibble::tibble(
+      Selection = df$selection,
+      View = 'Spectrogram 1',
+      Channel = 1,
+      `Begin File` = stringr::str_c(df$json, '.flac'),
+      `Begin Time (s)` = df$relative_time,
+      `End Time (s)` = df$relative_time + 3,
+      `Low Freq (Hz)` = 150,
+      `High Freq (Hz)` = 12000,
+      `Common Name` = df$common_name,
+      Score = df$score,
+      Detector = 'BirdNET',
+      Species = as.character(NA),
+      Keep = 'Y'
+    ) |>
+      write.table(
+        file = stringr::str_glue(here::here('code_outputs/forest_owl_selection_tables_{date_time}/{flac_name}.BirdNET')),
+        sep = "\t",
+        row.names = FALSE,
+        col.names = TRUE,
+        quote = FALSE
+      )
+
+  }
 
 }
