4dedd0055d9b355f424b5043491a329c6432d430
jaymwin <jmwiniarski@gmail.com>
2024-09-25 11:51:35 -0500

    fixed ownership function

--- R/cb_cells_by_ownership.R
+++ R/cb_cells_by_ownership.R
@@ -16,21 +16,21 @@ cb_cells_by_ownership <- function(ownership) {
   if (ownership == 'usfs') {
 
     cells <-
-      cb_get_spatial('hexes') |>
+      cb_get_spatial('sierra_hexes') |>
       dplyr::filter(stringr::str_detect(ownership, 'Forest')) |>
       dplyr::pull(cell_id)
 
   } else if (ownership == 'nps') {
 
     cells <-
-      cb_get_spatial('hexes') |>
+      cb_get_spatial('sierra_hexes') |>
       dplyr::filter(!stringr::str_detect(ownership, 'Forest')) |>
       dplyr::pull(cell_id)
 
   } else if (ownership == 'any') {
 
     cells <-
-      cb_get_spatial('hexes') |>
+      cb_get_spatial('sierra_hexes') |>
       dplyr::pull(cell_id)
 
   }
