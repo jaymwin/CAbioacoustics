6616dfe7d75fc636c28ff5d3c3e222bee29582bd
jaymwin <jmwiniarski@gmail.com>
2024-09-26 10:26:05 -0500

    add function to print Globus endpoint UUIDs

---
+++ R/cb_show_globus_endpoints.R
@@ -0,0 +1,14 @@
+
+#' Print Globus endpoint IDs
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+cb_show_globus_endpoints <- function() {
+
+  message(stringr::str_glue("Amazon S3 endpoint = {keyring::key_get('s3_globus_endpoint')}"))
+  message(stringr::str_glue("Research Drive endpoint = {keyring::key_get('research_drive_globus_endpoint')}"))
+
+}
