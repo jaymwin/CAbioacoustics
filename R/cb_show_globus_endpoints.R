b99cf09ca3c79d41fdd0f50c916aedecbe1fddd6
jaymwin <jmwiniarski@gmail.com>
2024-10-17 08:25:26 -0500

    added globus personal endpoint

--- R/cb_show_globus_endpoints.R
+++ R/cb_show_globus_endpoints.R
@@ -10,5 +10,6 @@ cb_show_globus_endpoints <- function() {
 
   message(stringr::str_glue("Amazon S3 endpoint = {keyring::key_get('s3_globus_endpoint')}"))
   message(stringr::str_glue("Research Drive endpoint = {keyring::key_get('research_drive_globus_endpoint')}"))
+  message(stringr::str_glue("Jay's laptop = {keyring::key_get('jmwiniarski_laptop_globus_endpoint')}"))
 
 }
