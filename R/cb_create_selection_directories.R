47a0b1bb474ff8f64fc6cb9939d235d91002e8c9
jaymwin <jmwiniarski@gmail.com>
2024-10-25 13:36:59 -0500

    modify for different species

--- R/cb_create_selection_directories.R
+++ R/cb_create_selection_directories.R
@@ -9,8 +9,17 @@
 #'
 #' @examples
 
-cb_create_selection_directories <- function(date_time, directory) {
+cb_create_selection_directories <- function(date_time, directory, species_group) {
 
-  fs::dir_create(stringr::str_glue(here::here('code_outputs/csow_bdow_selection_tables_{date_time}/{directory}')))
+  if (species_group != 'csow_bdow' & species_group != 'forest_owl') stop('must be `csow_bdow` or `forest_owl`')
+
+  if (species_group == 'csow_bdow') {
+
+    fs::dir_create(stringr::str_glue(here::here('code_outputs/csow_bdow_selection_tables_{date_time}/{directory}')))
+
+  } else if (species_group == 'forest_owl')
+
+    fs::dir_create(stringr::str_glue(here::here('code_outputs/forest_owl_selection_tables_{date_time}/{directory}')))
 
 }
+
