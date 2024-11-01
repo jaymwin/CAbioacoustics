20c583ce2bb8b9dd9d2e0fe716e1a04cc9f54188
jaymwin <jmwiniarski@gmail.com>
2024-11-01 13:08:08 -0500

    update read_birdnet_selection function

--- R/cb_read_birdnet_selection.R
+++ R/cb_read_birdnet_selection.R
@@ -33,7 +33,7 @@ cb_read_birdnet_selection <- function(birdnet_path) {
     dplyr::mutate(dplyr::across(c(View, `Begin File`, `Common Name`, Detector, Species, `Call Type`, Sex, Keep, Overwrite), as.character)) |>
     dplyr::mutate(dplyr::across(c(`Begin Time (s)`, `End Time (s)`, `Low Freq (Hz)`, `High Freq (Hz)`, Score), as.numeric)) |>
     # change blanks to NAs for now
-    dplyr::mutate(dplyr::across(c(View, `Begin File`, `Common Name`, Detector, Species, `Call Type`, Sex, Keep, Overwrite), ~dplyr::na_if(., ""))) |>
-    dplyr::mutate(dplyr::across(c(View, `Begin File`, `Common Name`, Detector, Species, `Call Type`, Sex, Keep, Overwrite), ~tidyr::replace_na(., "NA")))
+    dplyr::mutate(dplyr::across(c(View, `Begin File`, `Common Name`, Detector, Species, `Call Type`, Sex, Keep, Overwrite), ~dplyr::na_if(., ""))) # |>
+    # dplyr::mutate(dplyr::across(c(View, `Begin File`, `Common Name`, Detector, Species, `Call Type`, Sex, Keep, Overwrite), ~tidyr::replace_na(., "NA")))
 
 }
