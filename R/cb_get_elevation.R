b2b16b29581dbd5042c686c2104626c2ecb45224
jaymwin <jmwiniarski@gmail.com>
2024-11-13 14:46:38 -0600

    modified a bit. could be used for any continuous covariate and name changed.

--- R/cb_get_elevation.R
+++ R/cb_get_elevation.R
@@ -10,40 +10,72 @@
 #'
 #' @examples
 
-cb_get_elevation <- function(sf_object, unit, buffer = NULL) {
+cb_get_terrain <- function(sf_object, scale, terrain_layer, terrain_name, buffer_size = NULL, fun = NULL) {
 
-  if (unit == 'aru' & is.null(buffer) == TRUE) {
+  if (scale == 'aru' & is.null(buffer_size) == TRUE) {
+
+    terrain_df <- terra::extract(terrain_layer, deployments_sf |> sf::st_transform(sf::st_crs(terrain_layer)))
 
     sf_object <-
-      elevatr::get_elev_point(sf_object, src = "epqs") |>
-      dplyr::select(-elev_units)
+      sf_object |>
+      dplyr::bind_cols(terrain_df |> dplyr::select(2) |> dplyr::rename_at(1, ~ terrain_name))
 
   }
 
-  if (unit == 'aru' & is.null(buffer) == FALSE) {
+  if (scale == 'aru' & is.null(buffer_size) == FALSE) {
 
     # get data frame with elevation summarized at different buffer sizes
-    elev_df <-
-      buffer %>%
+    terrain_df <-
+      buffer_size %>%
       # create column names
-      stringr::str_c('elev', ., sep = '_') |>
+      stringr::str_c(terrain_name, ., sep = '_') |>
       purrr::set_names() |>
       # iterate through buffer sizes
       purrr::map_dfr(
         \(x)
         exactextractr::exact_extract(
-          ca_elev,
+          terrain_layer,
           # buffer points
-          sf_object |> sf::st_transform(sf::st_crs(ca_elev)) |> sf::st_buffer(as.numeric(stringr::str_extract(x, "\\d"))),
-          fun = 'mean'
+          sf_object |> sf::st_transform(sf::st_crs(terrain_layer)) |> sf::st_buffer(as.numeric(stringr::str_extract(x, "\\d+"))),
+          fun = fun,
+          progress = FALSE
         ),
-        .id = 'buffer'
+        .id = 'buffer_size'
       )
 
-    # bind to deployments and view
+    if (length(fun) == 1) {
+
+      # use single function name
+      terrain_df <-
+        terrain_df |>
+        dplyr::select_all(list(~ paste0(fun, '_', .)))
+
+    } else {
+
+      # otherwise combine function, buffers for naming columns
+      terrain_df <-
+        terrain_df |>
+        tidyr::pivot_longer(!buffer_size, names_to = "statistic", values_to = "value") |>
+        tidyr::pivot_wider(
+          names_from = c(statistic, buffer_size),
+          names_glue = "{statistic}_{buffer_size}",
+          values_from = value
+        ) |>
+        tidyr::unnest() |>
+        suppressWarnings()
+
+    }
+
+    # bind to deployments
     sf_object <-
       sf_object |>
-      dplyr::bind_cols(elev_df)
+      dplyr::bind_cols(terrain_df)
+
+  }
+
+  if (scale == 'hex' & is.null(buffer_size) == TRUE) {
+
+    print('do hex stuff, not done yet')
 
   }
 
