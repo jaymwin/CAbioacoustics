bf006efd64b126e1961e6a41c8546506c9236f2e
jaymwin <jmwiniarski@gmail.com>
2024-11-15 13:26:07 -0600

    add example

--- R/cb_get_audio_duration.R
+++ R/cb_get_audio_duration.R
@@ -7,6 +7,14 @@
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#' flac_duration_df <-
+#'   flac_df |>
+#'     # list of flac paths
+#'     pull(path) |>
+#'     # run in parallel
+#'     future_map_dfr(get_duration)
+#'     }
 
 cb_get_audio_duration <- function(path) {
 
