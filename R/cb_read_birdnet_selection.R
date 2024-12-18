f821883abfaa32c35db73ccc82c5e7164e95b141
jaymwin <jmwiniarski@gmail.com>
2024-12-18 12:57:42 -0600

    slight changes to read a regular BirdNET file or a CSOW/BDOW one

--- R/cb_read_birdnet_selection.R
+++ R/cb_read_birdnet_selection.R
@@ -25,15 +25,43 @@
 #'   purrr::map_dfr(cb_read_birdnet_selections)
 #' }
 
-cb_read_birdnet_selection <- function(birdnet_path) {
+cb_read_birdnet_selection <- function(birdnet_path, type) {
 
-  data.table::fread(birdnet_path) |>
-    tibble::as_tibble() |>
-    dplyr::mutate(dplyr::across(c(Selection, Channel), as.integer)) |>
-    dplyr::mutate(dplyr::across(c(View, `Begin File`, `Common Name`, Detector, Species, `Call Type`, Sex, Keep, Overwrite), as.character)) |>
-    dplyr::mutate(dplyr::across(c(`Begin Time (s)`, `End Time (s)`, `Low Freq (Hz)`, `High Freq (Hz)`, Score), as.numeric)) |>
-    # change blanks to NAs for now
-    dplyr::mutate(dplyr::across(c(View, `Begin File`, `Common Name`, Detector, Species, `Call Type`, Sex, Keep, Overwrite), ~dplyr::na_if(., ""))) # |>
-    # dplyr::mutate(dplyr::across(c(View, `Begin File`, `Common Name`, Detector, Species, `Call Type`, Sex, Keep, Overwrite), ~tidyr::replace_na(., "NA")))
+  # if (type == 'csow_bdow') {
+  #
+  #   data.table::fread(birdnet_path) |>
+  #     tibble::as_tibble() |>
+  #     dplyr::mutate(dplyr::across(c(Selection, Channel), as.integer)) |>
+  #     dplyr::mutate(dplyr::across(c(View, `Begin File`, `Common Name`, Detector, Species, `Call Type`, Sex, Keep, Overwrite), as.character)) |>
+  #     dplyr::mutate(dplyr::across(c(`Begin Time (s)`, `End Time (s)`, `Low Freq (Hz)`, `High Freq (Hz)`, Score), as.numeric)) |>
+  #     # change blanks to NAs for now
+  #     dplyr::mutate(dplyr::across(c(View, `Begin File`, `Common Name`, Detector, Species, `Call Type`, Sex, Keep, Overwrite), ~dplyr::na_if(., ""))) # |>
+  #   # dplyr::mutate(dplyr::across(c(View, `Begin File`, `Common Name`, Detector, Species, `Call Type`, Sex, Keep, Overwrite), ~tidyr::replace_na(., "NA")))
+  #
+  # } else if (is.null(type) == TRUE) {
+  #
+  #   readr::read_delim(birdnet_path)
+  #
+  # }
+
+  if (is.null(type) == TRUE) {
+
+    readr::read_delim(
+      birdnet_path,
+      delim = '\t',
+      col_types = 'icicddddcd'
+    )
+
+  } else if (type == 'csow_bdow') {
+
+    readr::read_delim(
+      birdnet_path,
+      delim = '\t',
+      col_types = 'icicddddcdccccc'
+    ) |>
+      # change blanks to NAs for now
+      dplyr::mutate(dplyr::across(c(View, `Begin File`, `Common Name`, Detector, Species, `Call Type`, Sex, Keep, Overwrite), ~dplyr::na_if(., "")))
+
+  }
 
 }
