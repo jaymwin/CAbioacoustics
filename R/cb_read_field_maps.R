4acb9d9436f5862a613072299b4623ca06ce0910
jaymwin <jmwiniarski@gmail.com>
2025-03-27 09:46:17 -0500

    add functions to clean up

--- R/cb_read_field_maps.R
+++ R/cb_read_field_maps.R
@@ -21,6 +21,31 @@ cb_read_field_maps <- function(feature_layer) {
       )
     )
   ) |>
-    janitor::clean_names()
+    janitor::clean_names() |>
+    dplry::select(
+      creation_date,
+      deployment_or_recovery,
+      deployed_by,
+      deployment_date_and_time,
+      swift_id,
+      sd_card_id,
+      group_id,
+      visit_id,
+      cell_id,
+      unit_number,
+      deployment_notes,
+      recovery_date_and_time,
+      recovered_by,
+      unit_missingnot_recovered,
+      recover_recording_status,
+      unit_broken,
+      microphone_missingdamaged,
+      microphone_windscreen_damaged,
+      recover_notes,
+      did_you_take_a_deployment_photo,
+      dplyr::matches('esri')
+    ) |>
+    dplyr::mutate(dplyr::across(dplyr::where(is.character), ~dplyr::na_if(., " "))) |>
+    sf::st_transform(4326)
 
 }
