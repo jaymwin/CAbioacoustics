f605a2f05122b674afcafa30d9c43f503e1e3707
jaymwin <jmwiniarski@gmail.com>
2025-03-27 09:25:32 -0500

    added function to read data from ArcGIS Field Maps

---
+++ R/cb_read_field_maps.R
@@ -0,0 +1,26 @@
+
+#' Read ARU deployment/recovery data from ArcGIS Field Maps
+#'
+#' @param feature_layer HTML to hosted Field Maps feature layer
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+cb_read_field_maps <- function(feature_layer) {
+
+  # make sure there's arcgis pro on your computer and it connects
+  arcgisbinding::arc.check_product()
+
+  # read now as sf object
+  arcgisbinding::arc.data2sf(
+    arcgisbinding::arc.select(
+      arcgisbinding::arc.open(
+        feature_layer
+      )
+    )
+  ) |>
+    janitor::clean_names()
+
+}
