69e2f780b1ca9e9739c0551c75369039ba53f6dd
jaymwin <jmwiniarski@gmail.com>
2025-05-28 10:48:21 -0500

    fix typo

--- R/cb_season_dates.R
+++ R/cb_season_dates.R
@@ -34,7 +34,7 @@ cb_season_dates <- function(study_type, cell_ids, start_year, end_year) {
   efforts_deployments <-
     efforts |>
     # deployment acoustic_field_visit_id = effort id; rename to join
-    dplyr::left_join(deployments |> dplyr::rename(acoustic_field_visit_id = id), by = 'acoustic_field_visit_id')
+    dplyr::left_join(deployments |> dplyr::rename(acoustic_field_visit_id = id), by = 'acoustic_field_visit_id') |>
     # dplyr::left_join(deployments |> dplyr::rename(acoustic_field_visit_id = id), by = dplyr::join_by('acoustic_field_visit_id')) |>
     dplyr::filter(
       cell_id %in% cell_ids,
